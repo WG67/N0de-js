eb7f41012bfa45d1fa86993ffcfc393d87928855
WG67 <Withergaming67@gmail.com>
2021-08-21 21:12:04 +0100

    App js and index html working yay

--- Project/app.js
+++ Project/app.js
@@ -1,5 +1,27 @@
 const http = require('http')
-const port = require('port')
+const fs = require('fs')
+const port = 3000
+
+const server = http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/html'});
+fs.readFile('index.html', function (error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('Error: file not found')
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
+});
+
+server.listen(port, function(error) {
+    if (error) {
+        console.log('Something Went Wrong', error)
+    } else {
+        console.log('Server is listning on Port' + port)
+    }
+});
 
 
 
